45504eb586139de9829270be1fbfa18373acd5e1
Dirivial <alexander.kadeby@gmail.com>
2023-01-22 08:59:59 +0100

    First post completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
+import remarkGfm from "remark-gfm";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx({
+    syntaxHighlighting: 'Prism',
+    remarkPlugins: [remarkGfm, remarkToc],
+    gfm: true,
+  },)]
 });
