10fea93ee73d330173c0675b97018e1c9ec35eec
Dirivial <alexander.kadeby@gmail.com>
2023-01-31 07:38:45 +0100

    Update!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,15 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
 import remarkGfm from "remark-gfm";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx({
     syntaxHighlighting: 'Prism',
-    remarkPlugins: [remarkGfm, remarkToc],
+    remarkPlugins: [remarkGfm, remarkToc, remarkMath],
+    rehypePlugins: [rehypeKatex],
     gfm: true,
   },)]
 });
